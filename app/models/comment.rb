95aa1b4d50572e327b777f2f6e2360ec02cd258b
Bartosz Nowak <sztosz@gmail.com>
2014-06-04 22:42:11 +0200

    Added code to comment model so it can comment_spec tests

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,28 @@
 class Comment
+
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :body, type: String
+  field :vote_value, type: Integer
+  field :abusive, type: Boolean, default: false
+  field :abusive_override, type: Boolean, default: false
+
+  validates_presence_of :body
+
+  belongs_to :user
+  belongs_to :post
+  has_many :votes, after_add: :mark_abusive
+
+  def mark_abusive(v)
+    if self.abusive_override
+    else
+      vote_value = 0
+      self.votes.each { |vote| vote_value += vote.value }
+      if vote_value <= -3
+        self.abusive = true
+      end
+    end
+  end
+
 end
