972fd1a09b88955c02d90cb0cd58c2d13a038b99
Bartosz Nowak <sztosz@gmail.com>
2014-06-04 23:09:17 +0200

    Added code to post model so it can pass post_spec tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,11 +10,33 @@ class Post
   validates_presence_of :body, :title
 
   belongs_to :user
+  has_many :comments
 
   default_scope ->{ ne(archived: true) }
 
   def archive!
     update_attribute :archived, true
+  end
+
+  def hotness
+    time_delta = ((Time.current - self.created_at).to_i / 3600).to_i
+    if time_delta <= 24
+      hotness_based_on_votes + 3
+    elsif 24 < time_delta && time_delta < 72  # damn it, why i can't just do elsif 24 < time_delta < 72 like in python?
+      hotness_based_on_votes + 2
+    elsif 72 <= time_delta && time_delta <= 168
+      hotness_based_on_votes + 1
+    else
+      hotness_based_on_votes
+    end
+  end
+
+  def hotness_based_on_votes
+    if self.comments.count >= 3
+      1
+    else
+      0
+    end
   end
 
 end
